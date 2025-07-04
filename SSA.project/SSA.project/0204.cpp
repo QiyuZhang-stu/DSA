9c930431bc4a28f5705c853745f0d8f436c8ae43
QiyuZhang-stu <zhangqiyu_0721@qq.com>
2025-07-04 19:36:41 +0800

    提交0204.cpp

---
+++ SSA.project/SSA.project/0204.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+//tool
+
+
+int main() {
+	//input
+	string s = "";
+	int len = 0;
+	while(cin>>len){
+		if (len == 0) break;
+		cin >> s;
+
+	}
+	return 0;
+}
