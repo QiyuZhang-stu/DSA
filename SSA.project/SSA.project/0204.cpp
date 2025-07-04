d50bad291d4a9921351ce98828ce6c8a4354a31d
QiyuZhang-stu <zhangqiyu_0721@qq.com>
2025-07-04 19:37:37 +0800

    提交0204.cpp

--- SSA.project/SSA.project/0204.cpp
+++ SSA.project/SSA.project/0204.cpp
@@ -1,18 +1,43 @@
-#include<iostream>
-
+#include <iostream>
+#include <vector>
+#include <cstring>
 using namespace std;
 
-//tool
-
+void computePrefixFunction(const string& s, vector<int>& pi) {
+    int n = s.length();
+    pi[0] = 0;
+    for (int i = 1; i < n; i++) {
+        int j = pi[i - 1];
+        while (j > 0 && s[i] != s[j]) {
+            j = pi[j - 1];
+        }
+        if (s[i] == s[j]) {
+            j++;
+        }
+        pi[i] = j;
+    }
+}
 
 int main() {
-	//input
-	string s = "";
-	int len = 0;
-	while(cin>>len){
-		if (len == 0) break;
-		cin >> s;
+    int caseNum = 0;
+    int n;
+    while (cin >> n && n != 0) {
+        string s;
+        cin >> s;
+        caseNum++;
+        cout << "Test case #" << caseNum << endl;
 
-	}
-	return 0;
+        vector<int> pi(n);
+        computePrefixFunction(s, pi);
+
+        for (int i = 1; i < n; i++) {
+            int len = i + 1;
+            int cycleLen = len - pi[i];
+            if (pi[i] > 0 && len % cycleLen == 0) {
+                cout << len << " " << len / cycleLen << endl;
+            }
+        }
+        cout << endl;
+    }
+    return 0;
 }
